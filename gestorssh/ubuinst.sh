24b321cddd55baa6c8e63e33dde8b466865f76b9
Jean Rocha <105602625+JeanRocha91x@users.noreply.github.com>
2022-08-11 20:40:50 -0300

    Update ubuinst.sh

--- gestorssh/ubuinst.sh
+++ gestorssh/ubuinst.sh
@@ -31,7 +31,7 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 chmod +x /usr/local/bin/composer
 cd /var/www/html || exit
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/gestorssh18.zip > /dev/null 2>&1
+wget https://github.com/JeanRocha91x/psshplus-/raw/main/gestorssh/gestorssh18.zip > /dev/null 2>&1
 apt-get install unzip > /dev/null 2>&1
 unzip gestorssh18.zip > /dev/null 2>&1
 chmod -R 777 /var/www/html
@@ -60,7 +60,7 @@ mv /tmp/pass /var/www/html/pages/system/pass.php
 }
 function inst_db { 
 cd || exit
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/bdgestorssh.sql > /dev/null 2>&1
+wget https://github.com/JeanRocha91x/psshplus-/raw/main/gestorssh/bdgestorssh.sql > /dev/null 2>&1
 sleep 1
 if [[ -e "$HOME/bdgestorssh.sql" ]]; then
     mysql -h localhost -u sshplus -p"$pwdroot" --default_character_set utf8 sshplus < bdgestorssh.sql
@@ -105,8 +105,8 @@ function fun_swap {
 }
 function tst_bkp {
 cd /bin || exit
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/userteste.sh
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/autobackup.sh
+wget https://github.com/JeanRocha91x/psshplus-/raw/main/gestorssh/userteste.sh
+wget https://github.com/JeanRocha91x/psshplus-/raw/main/gestorssh/autobackup.sh
 chmod 777 /bin/userteste.sh > /dev/null 2>&1
 chmod 777 /bin/autobackup.sh > /dev/null 2>&1
 mkdir /root/backupsql > /dev/null 2>&1
@@ -120,9 +120,9 @@ echo "America/Sao_Paulo" > /etc/timezone
 ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime > /dev/null 2>&1
 dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
 clear
-echo -e "\E[44;1;37m    INSTALAR PAINELWEB GESTOR-SSH     \E[0m" 
+echo -e "\E[44;1;37m    INSTALAR PAINELWEB SWIT-T     \E[0m" 
 echo ""
-echo -e "                 \033[1;31mBy @nandoslayer\033[1;36m"
+echo -e "                 \033[1;31mBy @swittecnologia\033[1;36m"
 echo ""
 read -p "DIGITE SUA SENHA ROOT: " pwdroot
 echo "root:$pwdroot" | chpasswd
@@ -140,7 +140,7 @@ cron_set
 fun_swap
 tst_bkp
 clear
-echo -e "\033[1;32m GESTOR-SSH INSTALADO COM SUCESSO!"
+echo -e "\033[1;32m P-SWIT-T INSTALADO COM SUCESSO!"
 echo ""
 echo -e "                 \033[1;31mBy @nandoslayer\033[1;36m"
 echo ""
@@ -154,7 +154,7 @@ echo -e "\033[1;36m LOJA DE APPS:\033[1;37m http://$IP/phpmyadmin\033[0m"
 echo -e "\033[1;36m USUÁRIO:\033[1;37m sshplus\033[0m"
 echo -e "\033[1;36m SENHA:\033[1;37m $pwdroot\033[0m"
 echo ""
-echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@nandoslayer\033[0m"
+echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@swittecnologia\033[0m"
 echo ""
 sed -i "s;upload_max_filesize = 2M;upload_max_filesize = 64M;g" /etc/php/7.4/apache2/php.ini > /dev/null 2>&1
 sed -i "s;post_max_size = 8M;post_max_size = 64M;g" /etc/php/7.4/apache2/php.ini > /dev/null 2>&1
