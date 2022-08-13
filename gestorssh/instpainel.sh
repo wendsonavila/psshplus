e09ac3a65e0319707cebf353083aba0f7974f720
Jean Rocha <105602625+JeanRocha91x@users.noreply.github.com>
2022-08-13 13:21:07 -0300

    Update instpainel.sh

--- gestorssh/instpainel.sh
+++ gestorssh/instpainel.sh
@@ -4,9 +4,9 @@ echo "America/Sao_Paulo" > /etc/timezone
 ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime > /dev/null 2>&1
 dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
 clear
-echo -e "\E[44;1;37m    INSTALAR PAINEL PWEB GESTOR-SSH     \E[0m" 
+echo -e "\E[44;1;37m    INSTALAR PAINEL PWEB SWIT-T     \E[0m" 
 echo ""
-echo -e "                 \033[1;31mBy @nandoslayer\033[1;36m"
+echo -e "                 \033[1;31mBy @swittecnologia\033[1;36m"
 echo ""
 echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
 clear
@@ -15,7 +15,7 @@ apt-get install figlet -y > /dev/null 2>&1
 rm /bin/pweb > /dev/null 2>&1
 sleep 5
 cd /bin || exit
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/pweb > /dev/null 2>&1
+wget https://github.com/JeanRocha91x/psshplus-/raw/main/gestorssh/pweb > /dev/null 2>&1
 chmod 777 pweb > /dev/null 2>&1
 clear
 mkdir /bin/ppweb > /dev/null 2>&1
@@ -36,6 +36,6 @@ clear
 echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • \033[0m"
 echo ""
 echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mpweb\033[0m"
-echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@nandoslayer\033[0m"
+echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@swittecnologia\033[0m"
 cat /dev/null > ~/.bash_history && history -c
 exit
