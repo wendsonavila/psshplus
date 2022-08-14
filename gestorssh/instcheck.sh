4b752d1d930f5adb26dbc9e6e717b9760daffb50
Jean Rocha <105602625+JeanRocha91x@users.noreply.github.com>
2022-08-14 16:34:08 +0100

    Update instcheck.sh

--- gestorssh/instcheck.sh
+++ gestorssh/instcheck.sh
@@ -6,7 +6,7 @@ dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
 clear
 echo -e "\E[44;1;37m    INSTALAR CHECKUSER CONECTA4G     \E[0m" 
 echo ""
-echo -e "                 \033[1;31mBy @nandoslayer\033[1;36m"
+echo -e "                 \033[1;31mBy @swittecnoloia\033[1;36m"
 echo ""
 echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
 clear
@@ -16,8 +16,8 @@ pip3 install flask > /dev/null 2>&1
 rm /bin/chuser > /dev/null 2>&1
 sleep 5
 cd /bin || exit
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/chuser > /dev/null 2>&1
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/userscheck > /dev/null 2>&1
+wget https://github.com/JeanRocha91x/psshplus-/raw/main/gestorssh/chuser > /dev/null 2>&1
+wget https://github.com/JeanRocha91x/psshplus-/raw/main/gestorssh/userscheck > /dev/null 2>&1
 chmod 777 chuser > /dev/null 2>&1
 chmod 777 userscheck > /dev/null 2>&1
 clear
@@ -27,7 +27,7 @@ echo -e 'By: @KIRITO_SSH' > /usr/lib/licence
 mkdir /usr/lib/checkuser > /dev/null 2>&1
 cd /usr/lib/checkuser || exit
 rm checkuser.py > /dev/null 2>&1
-wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/checkuser.py > /dev/null 2>&1
+wget https://github.com/JeanRocha91x/psshplus-/raw/main/gestorssh/checkuser.py > /dev/null 2>&1
 chmod 777 checkuser.py > /dev/null 2>&1
 clear
 echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • \033[0m"
@@ -36,6 +36,6 @@ clear
 echo ""
 echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mchuser\033[0m"
 echo ""
-echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@nandoslayer\033[0m"
+echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@swittecnologia\033[0m"
 cat /dev/null > ~/.bash_history && history -c
 exit
