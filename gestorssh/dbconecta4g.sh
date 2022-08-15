521a8c727ec8c6cec8a7b4ebc191390b3c632649
Jean Rocha <105602625+JeanRocha91x@users.noreply.github.com>
2022-08-15 23:05:57 +0100

    Update dbconecta4g.sh

--- gestorssh/dbconecta4g.sh
+++ gestorssh/dbconecta4g.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 clear
-echo ""
-echo -e "\033[1;36mCOLOQUE A MESMA SENHA DA INSTALAÇÃO \033[1;37m"
-echo ""
-apt install nano > /dev/null 2>&1
-nano /var/www/html/lib/Database/Connection.php > /dev/null 2>&1
+echo "America/Sao_Paulo" > /etc/timezone
+ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime > /dev/null 2>&1
+dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
 clear
+echo -e "\E[44;1;37m    COLOQUE A MESMA SENHA DA INSTALAÇÃO     \E[0m" 
+echo ""
+echo ""
+echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
+clear
+echo -e "\n\033[1;36mABRINDO O ARQUIVO \033[1;33mAGUARDE..."
+apt-get install nano > /dev/null 2>&1
+nano /var/www/html/lib/Database/Connection.php > /dev/null 2>&1
+sleep 5
+exit
 pweb
