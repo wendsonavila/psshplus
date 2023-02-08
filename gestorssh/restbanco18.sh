f9a3e3e947e7a912b812fd645c0e6335ccde6bc2
wendsonavila <103389432+wendsonavila@users.noreply.github.com>
2023-02-08 16:44:49 -0300

    Update restbanco18.sh

--- gestorssh/restbanco18.sh
+++ gestorssh/restbanco18.sh
@@ -32,9 +32,9 @@ clear
 mkdir /root/restaurar > /dev/null 2>&1
 clear
 echo ""
-echo -e "                              \033[1;31mBy @swittecnologia\033[1;36m"
+echo -e "                              \033[1;31mBy @WendsonReis\033[1;36m"
 echo ""
-echo -e "\033[1;33mRESTAURAR BANCO DE DADOS DO PAINEL SWIT-T!\033[0m" 
+echo -e "\033[1;33mRESTAURAR BANCO DE DADOS DO PAINEL WR-SSH!\033[0m" 
 echo ""
 echo -e "\n\033[1;33mÉ NECESSÁRIO O PAINEL INSTALADO E O\nAQUIVO (\033[1;32msshplus.sql\033[1;33m) NA PASTA (root/restaurar)!\033[0m\n"
 echo ""
@@ -73,7 +73,7 @@ pweb
 mysql -h localhost -u sshplus -p$passdb -e "drop database sshplus"
 mysql -h localhost -u sshplus -p$passdb -e 'CREATE DATABASE sshplus'
 mysql -h localhost -u sshplus -p$passdb --default_character_set utf8 sshplus < /root/restaurar/sshplus.sql
-echo -e "                              \033[1;31mBy @swittecnologia\033[1;36m"
+echo -e "                              \033[1;31mBy @WendsonReis\033[1;36m"
 echo ""
 echo -e "\n\033[1;32mDADOS RESTAURADO COM SUCESSO!\033[0m"
 echo ""
